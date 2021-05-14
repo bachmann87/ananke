94f871d3bc38089fe70d94045145751b50819291
Allan Bachmann <info@ajaybachmann.ch>
2021-05-14 14:39:49 +0200

    Added Login Function for Demostration Purposes

--- src/public/js/auth.js
+++ src/public/js/auth.js
@@ -1,3 +1,44 @@
+/**
+ * Diese Datei zeigt ein Refactoring Approach von mir. Die 
+ * Funktion login war die alte, bestehendene Funktion. Die
+ * asynchrone Funktion "authenticateUser" zeigt den neuen 
+ * Weg nach dem Refactoring basierend auf Fetch API.
+ * 
+ * 1.) Umstellung auf Asynchrones Verhalten
+ * 2.) Verwendung eines ServiceProvider, damit keine einzelne XMLHttpRequests vorhanden sind
+ * 3.) Basierend auf Promises 
+ * 4.) Umstellung auf CamelCase
+ * 5.) PortalService basierend auf Fetch API
+ */
+function login(form_obj) {
+
+    $("div#login").fadeOut();
+
+    var url_path = document.URL;
+    var login_successful = true;
+
+    $.ajax({
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        method: "POST",
+        data: form_parameter($(form_obj).attr("name")),
+        success: function(response) {
+            if(response.trim() == "first_login") {
+                window.location = "user_profile?first_login=1"
+            } else {
+                window.location = "login";
+            }
+        }
+    })
+
+    if(login_successful) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 async function authenticateUser(event, form) {
 
     event.preventDefault();
@@ -8,7 +49,7 @@ async function authenticateUser(event, form) {
     });
 
     if(response.ok) {
-        // Handle Client Redirect based on Response Route
+        window.location = response.redirect;
     }
 
 }
