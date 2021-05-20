90b78711f287e080e46a8951cdce09216a6b650a
Allan Bachmann <info@ajaybachmann.ch>
2021-05-20 19:46:00 +0200

    Added Google Fonts; Adjusted some Styles

--- src/public/index.php
+++ src/public/index.php
@@ -5,13 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Allink - Clean Code Principles</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Newsreader:wght@300&family=Roboto:wght@300&display=swap" rel="stylesheet"> 
     <link rel="stylesheet" href="css/master.min.css">
 </head>
 <body>
     <main>
         <div class="flex">
             <div class="flex-item">
-                <h1>I create web that please people</h1>
+                <h1 class=title>I create web that please people</h1>
                 <div>There are two parts to learning craftsmanship: knowledge and work. You must gain the knowledge of principles, patterns, practices, and heuristics that a craftsman knows, and you must also grind that knowledge into your fingers, eyes, and gut by working hard and
     practicing.</div>
             </div>
